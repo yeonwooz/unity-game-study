6b1688476701267cf5378bdae3a5855493c49cb7
Suyeon Woo <woosean999@gmail.com>
2025-05-19 23:01:01 +0900

    카메라 이동 부드럽게

--- Assets/TutorialInfo/Scripts/CameraMovement.cs
+++ Assets/TutorialInfo/Scripts/CameraMovement.cs
@@ -2,25 +2,37 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float rotationSpeed = 3.0f; 
+    public float rotationSpeed = 1.5f; 
     private Vector3 lastMousePosition;
+    private bool isDragging = false;
 
     void Update()
     {
-        if (Input.GetMouseButton(0))  
+        // 마우스 클릭 시작 시점
+        if (Input.GetMouseButtonDown(0))
         {
-            Vector3 delta = Input.mousePosition - lastMousePosition;  // 마우스 이동량 계산
             lastMousePosition = Input.mousePosition;
-
-            float rotationX = delta.x * rotationSpeed * Time.deltaTime; 
-            float rotationY = -delta.y * rotationSpeed * Time.deltaTime; 
-
-            transform.Rotate(Vector3.up, rotationX, Space.World);  
-            transform.Rotate(Vector3.right, rotationY, Space.Self); 
+            isDragging = true;
         }
-        else
+
+        // 마우스 클릭 중
+        if (Input.GetMouseButton(0) && isDragging)
         {
+            Vector3 delta = Input.mousePosition - lastMousePosition;
             lastMousePosition = Input.mousePosition;
+
+            float rotationX = delta.x * rotationSpeed * Time.deltaTime;
+            float rotationY = -delta.y * rotationSpeed * Time.deltaTime;
+
+            // 회전 적용
+            transform.Rotate(Vector3.up, rotationX, Space.World);
+            transform.Rotate(Vector3.right, rotationY, Space.Self);
+        }
+
+        // 마우스 클릭 종료 시점
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
         }
     }
 }
